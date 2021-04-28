0c7837b98fde4aeae37d01421d50792ffec9592b
FrancoRosa <km115.franco@gmail.com>
2021-04-28 15:58:48 -0500

    Erase return value after write

--- production/ccs811.py
+++ production/ccs811.py
@@ -258,8 +258,7 @@ def ccs811SetBaseline(baseline):
         global bus
         baselineMSB = (baseline >> 8) & 0xFF
         baselineLSB = baseline & 0xFF
-        result = ccs811NWriteRegisters(CCS811_BASELINE, [baselineMSB, baselineLSB])
-        return result
+        ccs811NWriteRegisters(CCS811_BASELINE, [baselineMSB, baselineLSB])
 
 def ccs811Begin(driveMode):
         global HWRST
